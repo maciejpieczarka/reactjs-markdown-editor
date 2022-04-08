1bae35a6776f70379923a5f43ca2750c47e53349
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2022-04-08 15:03:19 +0200

    Add dependencies and Create basic structure

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer>&copy;2022 - Reactjs Markdown Generator</footer>
+  )
+}
+
+export default Footer;
