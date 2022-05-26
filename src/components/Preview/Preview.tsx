d61d2f4e4ef57dfe2a8273bd520a81640473c5a9
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2022-05-26 22:52:50 +0200

    Create preview component and apply basic styling

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import styles from './preview.module.css';
+
 interface IPreviewProps {}
 
 const Preview: React.FC<IPreviewProps> = (props) => {
-    return <div className="text-container">Preview</div>;
+    return (
+        <div className={`text-container ${styles.preview_wrapper}`}>
+            Preview
+        </div>
+    );
 };
 
 export default Preview;
