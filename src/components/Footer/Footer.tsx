5239a0aa11f7a0ef9c73778fe1334c67a0e3a606
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2022-05-30 09:00:03 +0200

    Add footer component and Change dependencies

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,25 @@
 import React, { FC } from 'react';
+import { AiOutlineGithub } from 'react-icons/ai';
 
 import './footer.css';
 
 const Footer: FC = () => {
-  return (
-    <footer>&copy;2022 - Reactjs Markdown Generator</footer>
-  )
-}
+    return (
+        <footer>
+            <div className="socials">
+                <a
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://github.com/maciejpieczarka/reactjs-markdown-generator"
+                    className="social-link"
+                >
+                    Source code:
+                    <AiOutlineGithub className="social-icon" />
+                </a>
+            </div>
+            <div className="copy">&copy;2022 - Reactjs Markdown Generator</div>
+        </footer>
+    );
+};
 
 export default Footer;
