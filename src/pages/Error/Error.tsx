23fac53c964366672396bb83065158c5dbe2ff87
maciejpieczarka <63975664+maciejpieczarka@users.noreply.github.com>
2022-06-07 11:56:19 +0200

    Add 404 page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import errorImg from './../../assets/404_img.svg';
 
-const Error: React.FC = () => {
-  return (
-    <div>Error 404 | Not Found</div>
-  )
+// css
+import './error.css';
+
+interface IErrorProps {}
+
+const Error: React.FC<IErrorProps> = (props) => {
+    return (
+        <div className="error-container">
+            <div className="img-box">
+                <img src={errorImg} alt="Error 404 - Page Not Found" />
+            </div>
+            <div className="description-box">
+                <div className="title-404">Error: 404</div>
+                <div className="subtitle-404">Page Not Found</div>
+            </div>
+        </div>
+    );
 };
 
 export default Error;
